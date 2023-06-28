4047e5d2099a846a6b6c79fc666a8ab007a87a36
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-06-28 19:02:18 +0200

    fixing EditModal

--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -32,6 +32,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(200).json(updatedUser);
   } catch (error) {
     console.log(error);
-    return res.status(400).end();
+    return res.status(400).json({ message: (error as Error).message });
   }
 }
